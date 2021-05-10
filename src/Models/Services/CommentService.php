42befcb5daae1e764de6b3da13c004cb7316b0d1
walkerchiu <chenjen.chiou@gmail.com>
2021-05-10 14:58:17 +0800

    Feat (Function): Add countComments function

--- src/Models/Services/CommentService.php
+++ src/Models/Services/CommentService.php
@@ -15,4 +15,27 @@ class CommentService
     {
         $this->repository = App::make(config('wk-core.class.morph-comment.commentRepository'));
     }
+
+    /**
+     * @param Comment|Int $source
+     * @param User        $viewer
+     * @param User        $author
+     * @return Int
+     */
+    public function countComments($source, $viewer = null, $author = null)
+    {
+        $comment_id = is_integer($source) ? $source : $source->id;
+
+        $data = [
+            'morph_type' => config('wk-core.class.morph-comment.comment'),
+            'morph_id'   => $comment_id
+        ];
+
+        if ($author)
+            $data['user_id'] = $author->id;
+
+        return App::make(config('wk-core.class.morph-comment.commentRepository'))
+                    ->whereByArray($data)
+                    ->count();
+    }
 }
